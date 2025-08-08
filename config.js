d1425a266249f22e2ce4ef4908bcd3f7ddd4a46b
TØP BØ¥ <nwankwogoodluck156@gmail.com>
2025-08-08 09:07:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.anitav4 = process.env.BOT_NAME || "ℂ𝕆ℕ𝕊ℙ𝕀ℝ𝔼𝕏 💫";
+global.ownername = process.env.OWNER_NAME || "🆃🅾🅿-🅱🅾🆈";
+global.packname = process.env.PACK_NAME || "ℂ𝕆ℕ𝕊ℙ𝕀ℝ𝔼𝕏 💫";
+global.author = process.env.AUTHOR || "🆃🅾🅿-🅱🅾🆈";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
